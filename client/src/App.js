75b56c179ab80ba59060b4816a93647270674b77
Benjamin Wright <bwright@trilogyed.com>
2019-11-17 21:35:34 -0500

    Customized app name in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// TODO: write auth provider
+// TODO: Only show authenticated app if provider displayse logged in
+// TODO: only show unauthenticated app if login is false
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Get Baking App</h1>
       </header>
     </div>
   );
