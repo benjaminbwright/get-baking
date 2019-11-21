75c3774b47342ad0d3f81adf603d345e7d9259be
Benjamin Wright <bwright@trilogyed.com>
2019-11-20 19:35:47 -0500

    Authenticated and unauthenticated app components

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,36 @@ import './App.css';
 // TODO: Only show authenticated app if provider displayse logged in
 // TODO: only show unauthenticated app if login is false
 
+// Authenticated app
+const AuthenticatedApp = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>The Get Baking App</h1>
+    </header>
+  </div>
+);
+
+// Unauthenticated app
+const UnauthenticatedApp = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>The Get Baking App</h1>
+      <p>U ain't logged in!?! Get outta here!!!</p>
+    </header>
+  </div>
+);
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Get Baking App</h1>
-      </header>
-    </div>
+  
+  const loggedIn = true;
+
+  return loggedIn ? (
+    <AuthenticatedApp />
+  ) : (
+    <UnauthenticatedApp />
   );
+  
 }
 
 export default App;
