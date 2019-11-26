2eacf48798eea093ae11d5ada2789c85b521adc6
Benjamin Wright <bwright@trilogyed.com>
2019-11-25 23:18:35 -0500

    setup input handler on login form

--- client/src/components/UnauthenticatedApp.js
+++ client/src/components/UnauthenticatedApp.js
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
+
+const LoginForm = props => {
+  return(
+    <div id="login-form-container">
+      <form id="login-form">
+        <input 
+          name="userName"
+          type="text"
+          onChange={props.handleInputChange} 
+          value={props.userName}
+        />
+        <input 
+          name="password"
+          type="password"
+          onChange={props.handleInputChange}
+          value={props.password}
+        />
+      </form>
+    </div>
+  );
+}
+
 
 // Unauthenticated app
-const UnauthenticatedApp = (props) => (
-  <div className="App">
-    <header className="App-header">
-      <h1>The Get Baking App</h1>
-      <button onClick={props.login} >Login</button>
-      <button>Register</button>
-    </header>
-  </div>
-);
+class UnauthenticatedApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: ""
+    }
+  }
+
+  handleInputChange = event => {
+
+    const { name, value } = event.target;
+  
+    this.setState({
+      [name]: value
+    });
+  }
+  
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>The Get Baking App</h1>
+          <LoginForm userName={this.state.userName} password={this.state.password} handleInputChange={this.handleInputChange} />
+          <button onClick={this.state.login} >Login</button>
+          <button>Register</button>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default UnauthenticatedApp
